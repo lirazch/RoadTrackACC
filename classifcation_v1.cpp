4dc368407a837fe1d6069a1d537c43b871dacb72
shgidi <shgidi@gmail.com>
2018-04-26 15:49:13 +0300

    working shipped model prototype

--- classifcation_v1.cpp
+++ classifcation_v1.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 #include <Eigen/Dense>
 #include <map>
+
+#include <fstream>
+#include <vector>
+#include <iterator>
+#include <string>
+#include <algorithm>
+#include <boost/algorithm/string.hpp>
+
 using namespace Eigen;
 using namespace std;
 
@@ -17,170 +25,168 @@ double* sigmoid (VectorXf a) //the sigmoid
 map<string, float> param_dict() //the map dict functionallity
 {
 	// make the dict of the parameters
-
 	//dict example from from stack overflow
-	// prints: Found! Foo is bar
 
 	map<string, float> my_map ;
-	my_map.insert(make_pair("f_mean_0",-2.97));
-	my_map.insert(make_pair("f_mean_5", -1.32));
-	my_map.insert(make_pair("f_mean_6", -4.56));
-	my_map.insert(make_pair("f_std_0", 23.43));
-	my_map.insert(make_pair("f_std_5", 32.57));
-	my_map.insert(make_pair("f_std_6", 12.59));
-	my_map.insert(make_pair("r_mean_0", -0.52));
-	my_map.insert(make_pair("r_mean_5", 11.32));
-	my_map.insert(make_pair("r_mean_6", 6.28));
-	my_map.insert(make_pair("r_std_0", 14.32));
-	my_map.insert(make_pair("r_std_5", 42.48));
-	my_map.insert(make_pair("r_std_6", 26.4));
-
-	//std::cout<<my_map.find("r_std_6");
-	//std::cout << "a => " << my_map.find('r_std_6')->second << '\n';
-		// searching
-	map<string, float>::iterator it;
-	it = my_map.find("r_std_6");
-	if (it != my_map.end())
-		cout << "Found! " << it->first << " is " << it->second << "\n";
-
-	/// there is more but later...
+	my_map.insert(make_pair("mean_f0",-2.97));
+	my_map.insert(make_pair("mean_f1", -1.32));
+	my_map.insert(make_pair("mean_f2", -4.56));
+	my_map.insert(make_pair("std_f0", 23.43));
+	my_map.insert(make_pair("std_f1", 32.57));
+	my_map.insert(make_pair("std_f2", 12.59));
+	my_map.insert(make_pair("mean_v0", -0.52));
+	my_map.insert(make_pair("mean_v1", 11.32));
+	my_map.insert(make_pair("mean_v2", 6.28));
+	my_map.insert(make_pair("std_v0", 14.32));
+	my_map.insert(make_pair("std_v1", 42.48));
+	my_map.insert(make_pair("std_v2", 26.4));
 
 	return my_map;
-}
-
-int ex () //another example for map/dict
-{
-  std::map<char,int> mymap;
-  std::map<char,int>::iterator it;
-
-  mymap['a']=50;
-  mymap['b']=100;
-  mymap['c']=150;
-  mymap['d']=200;
-
-  it = mymap.find('b');
-  if (it != mymap.end())
-    mymap.erase (it);
-
-  // print content:
-  std::cout << "elements in mymap:" << '\n';
-  std::cout << "a => " << mymap.find('a')->second << '\n';
-  std::cout << "c => " << mymap.find('c')->second << '\n';
-  std::cout << "d => " << mymap.find('d')->second << '\n';
-
-  return 0;
 }
 
 MatrixXf model_weights()//return the "model weights", currently 3X6
 {
 	MatrixXf m(3, 6);
-    VectorXf data_point(3);
-    m(0,0)=0.2030422;
+
+    m(0,0)=-0.2030422;
     m(0,1)=0.42991926;
     m(0,2)=-0.08137797;
-    m(0,3)=1;
-    m(0,4)=1;
-    m(0,5)=1;
+    m(0,3)=-0.0535;
+    m(0,4)=-0.0157;
+    m(0,5)=-0.0151;
     m(1,0)=0.11759804;
     m(1,1)=-0.10445514;
     m(1,2)=-0.003818286;
-    m(1,3)=1;
-    m(1,4)=1;
-    m(1,5)=1;
+    m(1,3)=0.008;
+    m(1,4)=0.0076;
+    m(1,5)=0.006;
     m(2,0)=0.03010658;
     m(2,1)=-0.61413439;
     m(2,2)=0.23996983;
-    m(2,3)=1;
-    m(2,4)=1;
-    m(2,5)=1;
+    m(2,3)=-0.09;
+    m(2,4)=0.0025;
+    m(2,5)=0.0125;
     return m;
 }
 
-int generator(MatrixXf m)
+VectorXf model_intercept() // read from ... CSV?
 {
-	//random numbers
+	VectorXf i(3);
+
+	i(0)=-1.25742184;
+	i(1)= -0.96226628;
+	i(2)=  0.24797999;
+	return i;
+}
+
+char get_sequence_length() //returns random length of currnet category from 100 to 500
+{
 	int v1 ;
-	int cat;
-	//MatrixXf m(3, 6);
-	VectorXf data_point(6);
-	VectorXf feature_layer(3);
-
-    for( int a = 0; a < 10; a = a + 1 ) 
+	char cat;
+    //for( int a = 0; a < 10; a = a + 1 ) 
     	// this will be the generator of the phases
-    {
-    	v1 = rand() % 400 +100;
-    	cat = rand() % 3;
 
+	v1 = rand() % 400 +100;
+	//cat = rand() % 3;
+	
+  	cat = (rand() % 3) + '0';
+
+    return cat;
+}
+
+
+double predict_logistic(VectorXf data_point) //predict logisitc regression given data points and weight
+{
+	VectorXf feature_layer(CATEGORY_COUNT);
+	VectorXf intercept(CATEGORY_COUNT);
+	MatrixXf weights(CATEGORY_COUNT, FEATURE_COUNT);
+
+	intercept=model_intercept();
+	weights = model_weights();
+    feature_layer = weights * data_point + intercept;
+
+	// 6*3*3 = 6
+    double* sig_vector = sigmoid(feature_layer);
+    double max=std::numeric_limits<int>::min();
+
+    //cout<<"feature"<<feature_layer;
+    int ref=-1;
+    for (int i = 0; i < 3; i++) //this is my hand made argmax
+    {
+    	///cout<<max<<"vec "<<sig_vector[i];
+    	if (max<sig_vector[i])
+    	{
+    		max=sig_vector[i];
+    		ref=i;
+    	}
     }
+    cout <<"\n"<<"predicted categroy w/ logreg is: " <<ref<<"\n";
+}
 
-    // no generate stream
-    map<string, float> stream_parms=param_dict();
+// A class to read data from a csv file.
 
-    std::normal_distribution<double> distribution(5.0,2.0);
-    std::default_random_engine generator;
-    std::cout << "normal_distribution (5.0,2.0):" << std::endl;
+class CSVReader
+{
+  std::string fileName;
+  std::string delimeter;
 
-    int p[10]={};
+public:
+  CSVReader(std::string filename, std::string delm = ",") :
+      fileName(filename), delimeter(delm)
+  { }
 
-    for (int i=0; i<1000; ++i) 
-    {
-    	double forward_m = distribution(generator);
-    	double vertical_m = distribution(generator);
-    	double radial_m = distribution(generator);
+  // Function to fetch data from a CSV File
+  std::vector<std::vector<std::string> > getData();
+};
 
-    	double forward_s = distribution(generator);
-    	double vertical_s = distribution(generator);
-    	double radial_s = distribution(generator);
-    	//if ((number>=0.0)&&(number<10.0)) ++p[int(number)];
-    	//cout<<number<<'\n'; // print these numbers rand
-	    data_point(0)=forward_m;
-	    data_point(1)=vertical_m;
-	    data_point(2)=radial_m;
-	    data_point(3)=forward_s;
-	    data_point(4)=vertical_s;
-	    data_point(5)=radial_s;
 
-	    //cout<<data_point<<"vector\n";
-	    
+// Parses through csv file line by line and returns the data
+// in vector of vector of strings.
 
-	    // now do sigmoid
-	    feature_layer=m * data_point;
-	    cout << (feature_layer) << "fig\n";
+std::vector<std::vector<std::string> > CSVReader::getData()
+{
+  std::ifstream file(fileName);
 
-	    //cout<<m*data_point<<"preds"; // 6*3*3 = 6
-	    double* sig_vector = sigmoid(feature_layer);
-	    double max=std::numeric_limits<int>::min();
-	    int ref=-1;
-	    for (int i = 0; i < 3; i++) //this is my hand made argmax
-	    {
-	    	//cout<<i<<" xx "<<m3[i]<<endl;
-	    	//cout<<"true"<<m3[i];
-	    	if (max<sig_vector[i])
-	    	{
-	    		max=sig_vector[i];
-	    		ref=i;
-	    	}
-	    }
+  std::vector<std::vector<std::string> > dataList;
 
-	    cout <<"categroy is: " <<ref<<"\n";
-	    //distance(feature_layer, max_element(feature_layer, feature_layer + N))
-		//cout << "Index of max element: " << distance(feature_layer, max_element(feature_layer, feature_layer + N)) << endl;
-  	}
+  std::string line = "";
+  // Iterate through each line and split the content using delimeter
+  while (getline(file, line))
+  {
+    std::vector<std::string> vec;
+    boost::algorithm::split(vec, line, boost::is_any_of(delimeter));
+    dataList.push_back(vec);
+  }
+  // Close the File
+  file.close();
+
+  return dataList;
 }
 
 int main()
 {
     //Matrix3f mat;
 
-    MatrixXf weights(CATEGORY_COUNT, FEATURE_COUNT);
+    VectorXf data_point(FEATURE_COUNT);
 
+	CSVReader reader("simulated_data.csv");
 
-  //mat = m.transpose()*m;
-    //std::cout << "Now mat is mat:\n" << m.transpose()*data_point << std::endl; example for matrix multiplication
-    weights = model_weights();
-    generator(weights);
+	// Get the data from CSV File
+	vector<vector<string> > dataList = reader.getData();
+
+	// Print the content of row by row on screen
+	
+	for(vector<string> vec : dataList)
+	{
+		int i=0; //counter
+	    for(string data : vec)
+	    {
+	        if (i>0) //skip first number (index)
+	        	data_point(i-1) = strtod((data).c_str(),0);
+	        	i+=1;
+	    }
+	    predict_logistic(data_point);
+	}
+	
 
 }
-
-//fucntion that returns a weight matrix
-// function that returns a stream
